93cfb1f42f11936ee8347175a8794e1fba4f12e8
白石大輔 <shiraishidaisuke@pro.local>
2017-07-27 23:43:43 +0900

    first commit.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dandelion
+
+### インストール
+
+```npm install -g vue-cli```   
+```vue init webpack .```   
+```npm install```  
+```npm install element-ui -D```  
+
+### 実行
+```npm run dev```  
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <main-header></main-header>
+    <el-input placeholder="Please input userid" v-model="userid" ></el-input>
+    <el-input placeholder="Please input password" v-model="password" type="password"></el-input>
+    <router-view></router-view>
+    <main-footer></main-footer>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      userid: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
